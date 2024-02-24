5ae977b8060aa59d81b1d3b14ac39d6b62dd0035
harishmuleva <harish_muleva@dell.com>
2024-02-24 20:11:49 +0530

    replace hphworkshop with myhphworkshop

---
+++ myhphworkshop/src/App.jsx
@@ -0,0 +1,56 @@
+
+import { Refine } from "@refinedev/core";
+import routerProvider, {
+    NavigateToResource
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import {
+    useNotificationProvider,
+    Layout,
+    ErrorComponent,
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { PostList } from "./pages/posts";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Refine
+                dataProvider={dataProvider(API_URL)}
+                routerProvider={routerProvider}
+                resources={[
+                    {
+                        name: "posts",
+                      
+                    },
+                ]}
+
+            >
+                <Routes>
+                    <Route
+                        element={
+                            <Layout>
+                                <Outlet />
+                            </Layout>
+                        }
+                    >
+                        <Route
+                            index
+                            element={<NavigateToResource resource="posts" />}
+                        />
+
+                        <Route path="posts">
+                            <Route index element={<PostList />} />
+                        </Route>
+                    </Route>
+                </Routes>
+            </Refine>
+        </BrowserRouter>
+    );
+};
+
+export default App;
